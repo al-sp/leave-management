e5da7272187191911ec52d612e42214abce39b6b
al-sp <alexandr.spov@gmail.com>
2020-08-06 22:08:32 +0300

    leavetype model

---
+++ leave-management/Controllers/LeaveAllocationController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class LeaveAllocationController : Controller
+    {
+        private readonly ILeaveTypeRepository _leavetyperepo;
+        private readonly ILeaveAllocationRepository _leaveallocationrepo;
+        private readonly IMapper _mapper;
+
+        public LeaveAllocationController(
+            ILeaveTypeRepository leavetyperepo,
+            ILeaveAllocationRepository leaveallocationrepo,
+            IMapper mapper
+            )
+        {
+            _leavetyperepo = leavetyperepo;
+            _leaveallocationrepo = leaveallocationrepo;
+            _mapper = mapper;
+        }
+
+        // GET: LeaveAllocationController
+        public ActionResult Index()
+        {
+            var leavetypes = _leavetyperepo.FindAll().ToList();
+            var model = _mapper.Map<List<LeaveType>, List<LeaveTypeVM>>(leavetypes);
+            return View(model);
+        }
+
+        // GET: LeaveAllocationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveAllocationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveAllocationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveAllocationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveAllocationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- leave-management/Models/LeaveTypeVM.cs
+++ leave-management/Models/LeaveTypeVM.cs
@@ -11,6 +11,10 @@ namespace leave_management.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Default Number Of Days")]
+        [Range(1, 28, ErrorMessage = "Please Enter A Valid Number (1 - 28)")]
+        public int DefaultDays { get; set; }
         [Display(Name = "Date Created")]
         public DateTime? DateCreated { get; set; }
     }
